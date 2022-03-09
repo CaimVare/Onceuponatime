fa30cbbcecda684ac3ba0dd7212ee75abbe9eb54
CaimVare <tatusya0033@gmail.com>
2022-03-09 21:29:10 +0900

    カーソルManagerで選択前のカーソル2を透明化

--- Assets/Scripts/UI/cursorController.cs
+++ Assets/Scripts/UI/cursorController.cs
@@ -6,12 +6,21 @@ using UnityEngine.UI;
 
 public class cursorController : MonoBehaviour
 {
+    public enum status
+    {
+        preSelect = 0,//選択前
+        select,//選択可能
+        DoneWait,//選択後待機
+        Done//選択終了
+    }
     GameObject cursorPrefab;//カーソル1宣言
+    GameObject cursorPrefab2;//カーソル2宣言
     public GameObject[] Targets { get; private set; }//カーソル移動用
     public int CurrentPos { get; private set; }//カーソル移動用
-    int select;//エンターフラグ用
+    public status state;//カーソルステータス管理
+    //public GameObject serected;
 
-    public void Start()
+    private void Start()
     {
         //カーソル移動用の空オブジェクト配列
         string[] name = { "samuraicursor", "ninjacursor", "mikocursor", "onmyoujicursor", "Completioncursor" };
@@ -23,58 +32,77 @@ public class cursorController : MonoBehaviour
         transform.position = Targets[CurrentPos].transform.position;
         //カーソル1を探す
         this.cursorPrefab = GameObject.Find("cursorPrefab");
-        //エンターフラグ
-        select = 0;
+        //カーソル2を探す
+        this.cursorPrefab2 = GameObject.Find("cursorPrefab2");
     }
     public void Update()
     {
-        if (select == 0)
+        switch (state)
         {
-            //上矢印で配列の1つ上に移動
-            if (Input.GetKeyDown(KeyCode.UpArrow))
-            {
-                if (CurrentPos > 0) CurrentPos--;
-            }
-            //下矢印で配列の1つ下に移動
-            if (Input.GetKeyDown(KeyCode.DownArrow))
-            {
-                if (CurrentPos < Targets.Length - 1) CurrentPos++;
-            }
-            transform.position = Targets[CurrentPos].transform.position;
-
-
-            //Enterを押したとき
-            if (Input.GetKeyDown(KeyCode.Return))
-            {
-                //カーソルの場所
-                float cursorPos = this.cursorPrefab.transform.position.y;
-                Debug.Log(cursorPos);
-                //半透明にする
-                this.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 0.5f);
-                GameObject selected = null;//キャラ選択を初期化
-                //配列からｙ軸が同じだったオブジェクトを返す
-                for (int i = 0; i < Targets.Length; i++)
+            case status.preSelect://選択前処理
                 {
-                    if (cursorPos == Targets[i].transform.position.y)
+                    //透明にする
+                    this.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 0);
+                    break;
+                 }
+
+            case status.select://選択可能処理
+                {
+                    //上矢印で配列の1つ上に移動
+                    if (Input.GetKeyDown(KeyCode.UpArrow))
                     {
-                        selected = Targets[i];
-                        Debug.Log(Targets[i]);
+                        if (CurrentPos > 0) CurrentPos--;
                     }
+                    //下矢印で配列の1つ下に移動
+                    if (Input.GetKeyDown(KeyCode.DownArrow))
+                    {
+                        if (CurrentPos < Targets.Length - 1) CurrentPos++;
+                    }
+                    transform.position = Targets[CurrentPos].transform.position;
+
+                    //Enterを押したとき
+                    if (Input.GetKeyDown(KeyCode.Return))
+                    {
+                        //カーソルの場所
+                        float cursorPos = this.cursorPrefab.transform.position.y;
+                        Debug.Log(cursorPos);
+                        //半透明にする
+                        this.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 0.5f);
+                        GameObject selected = null;//キャラ選択を初期化
+                                                   //配列からｙ軸が同じだったオブジェクトを返す
+                        for (int i = 0; i < Targets.Length; i++)
+                        {
+                            if (cursorPos == Targets[i].transform.position.y)
+                            {
+                                selected = Targets[i];
+                                Debug.Log(Targets[i]);
+                            }
+                        }
+                        //選択後待機に移行
+                        state = status.DoneWait;
+                        //BattleManagerに値を返す
+                    }
+                    break;
                 }
-                //エンターフラグに1を足す
-                select += 1;
-                //BattleManagerに値を返す
-                //カーソル2に移行
-            }
+            case status.DoneWait://選択後待機処理
+                {
+                    //選択解除
+                    if (Input.GetKeyDown(KeyCode.LeftArrow))
+                    {
+                        //選択可能に戻す
+                        state = status.select;
+                        //透過を戻す
+                        this.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 1);
+                        //BattleManagerに値を返す
+                    }
+                    break;
+                }
+            case status.Done:
+                {
+                    break;
+                }
         }
-        //選択解除
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-            {
-             select = 0;
-             //透過を戻す
-             this.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 1);
-            //BattleManagerに値を返す
-            }
+        
     }
     
     
--- Assets/Scripts/UI/cursorManager.cs
+++ Assets/Scripts/UI/cursorManager.cs
@@ -4,14 +4,29 @@ using UnityEngine;
 
 public class cursorManager : MonoBehaviour
 {
-    //現在どのカーソルを操作しているか
-    private int nowCursor;
-    //操作可能なカーソル
-    [SerializeField]
-    private List<GameObject> cursorList;
+    //選択カーソルステータス
+    private enum status
+    {
+        SelectOne = 0,
+        SelectTwo
+    }
+    //カーソル1を宣言
+    private GameObject cursor1;
+    //カーソル2を宣言
+    private GameObject cursor2;
+    private status state;
     void Start()
     {
-        
+        //初期選択カーソルを1に初期化
+        state = status.SelectOne;
+        //カーソル1を探す
+        cursor1 = GameObject.Find("cursorPrefab");
+        //カーソル2を探す
+        cursor2 = GameObject.Find("cursorPrefab2");
+        //カーソル1を選択可能に初期化
+        cursor1.GetComponent<cursorController>().state = cursorController.status.select;
+        //カーソル2を選択前に初期化
+        cursor2.GetComponent<cursorController>().state = cursorController.status.preSelect;
     }
 
     void Update()
